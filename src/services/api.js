47c444bd158dcae56d7c4b7e5bbacc3fa6393e4e
oXavier17 <ghenrique.xavier@hotmail.com>
2026-05-17 15:18:55 -0300

    teste

--- src/services/api.js
+++ src/services/api.js
@@ -1,29 +1,14 @@
 import axios from "axios";
 
-console.log("API URL:", import.meta.env.VITE_API_URL)
+const API_URL = import.meta.env.VITE_API_URL;
+
+console.log("ENV:", import.meta.env);
+console.log("API_URL:", API_URL);
+
+alert(API_URL);
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+    baseURL: API_URL,
 });
-
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            localStorage.removeItem("token");
-            localStorage.removeItem("usuario");
-            window.location.href = "/login";
-        }
-        return Promise.reject(error);
-    }
-);
 
 export default api;
