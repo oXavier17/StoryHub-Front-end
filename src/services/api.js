62244e8e605c403edf65e1d84f36d39ed7cc58fd
oXavier17 <ghenrique.xavier@hotmail.com>
2026-05-17 11:59:18 -0300

    at

--- src/services/api.js
+++ src/services/api.js
@@ -24,4 +24,10 @@ api.interceptors.response.use(
     }
 );
 
+console.log("API URL:", import.meta.env.VITE_API_URL)
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+});
+
 export default api;
