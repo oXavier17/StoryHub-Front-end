3832785a4eb0d4e1bfcf86cff6f957235bf91dc9
oXavier17 <ghenrique.xavier@hotmail.com>
2026-05-17 19:55:50 -0300

    correcao api.js

--- src/services/api.js
+++ src/services/api.js
@@ -15,7 +15,8 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
     (response) => response,
     (error) => {
-        if (error.response?.status === 401) {
+        const isAuthRoute = error.config?.url?.includes("/auth/")
+        if (error.response?.status === 401 && !isAuthRoute) {
             localStorage.removeItem("token");
             localStorage.removeItem("usuario");
             window.location.href = "/login";
